3c6696b3d586c5dd4564fc253eeadfd023ee11d9
Faddah Wolf <my_biz@me.com>
2015-11-18 22:00:59 -0800

    half-way through modules - added appid to  for accessing the API key for openweathermap.org and have temperature correctly displaying now, converted from Kelvin to Farenheit; still can't get weather and description data members to show, working on that, and need to get routes working for more days to show and convert date.

---
+++ app.js
@@ -0,0 +1,62 @@
+// MODULE
+
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
+
+// ROUTES
+
+weatherApp.config(function($routeProvider) {
+  
+  $routeProvider
+  
+  .when('/', {
+    templateUrl: 'pages/home.htm',
+    controller: 'homeController'
+  })
+  .when('/forecast', {
+    templateUrl: 'pages/forecast.htm',
+    controller: 'forecastController'
+  });
+  
+});
+
+// SERVICES
+
+weatherApp.service('cityService', function() {
+  
+  this.city = 'Portland, OR';
+  
+});
+
+// CONTROLLERS
+
+weatherApp.controller('homeController', ['$scope', '$log', '$routeParams', 'cityService', function($scope, $log, cityService) {
+  
+  $scope.city = cityService.city;
+  
+  $scope.$watch('city', function() {
+    cityService.city = $scope.city;
+  });
+  
+}]);
+
+weatherApp.controller('forecastController', ['$scope', '$log', '$resource', '$routeParams', 'cityService', function($scope, $log, $resource, $routeParams, cityService) {
+  
+  $scope.city = cityService.city;
+  
+  $scope.days = $routeParams.days || 2;
+  
+  $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" } } );
+  
+  $scope.weatherAPI.appid = 'f857b7ab2bf18470438f14cd1da0781f';
+  
+  $scope.weatherResult = $scope.weatherAPI.get({ appid: $scope.weatherAPI.appid, q: $scope.city, cnt: $scope.days });
+  
+  // $log.info($scope.weatherResult);
+  
+  $scope.convertToFarenheit = function(degK) {
+    
+    return Math.round((1.8 * (degK - 273)) + 32);
+    
+  };
+  
+}]);
